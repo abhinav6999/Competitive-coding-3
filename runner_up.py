8a29b56d9617736f5090d3da32859fabe76275ab
abhinav6999 <89853717+abhinav6999@users.noreply.github.com>
2021-08-31 21:22:58 +0530

    Update runner_up.py

--- runner_up.py
+++ runner_up.py
@@ -4,9 +4,9 @@ my_arr=list(arr)
 my_arr.sort()
 l1=my_arr
 l1.reverse()
-l2=l1
+l=l1
 print(l2)
 for i in range(n):
-    if(l2[i]>l2[i+1]):
+    if(l[i]>l[i+1]):
         print(l2[i+1])
         break
